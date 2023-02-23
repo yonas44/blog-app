d10201e04e545781c518d6a4b9e5b3a42f0bff79
yonas44 <tesfu.yonas1350@gmail.com>
2023-02-23 17:34:01 +0300

    Add associations for the models and also add custom methods for them

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,26 @@
 class Post < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :likes
+  has_many :comments
+
+  after_initialize do |_post|
+    update_user_post_counter unless Post.exists?(id: id)
+  end
+
+  def increment_likes_counter
+    update(likes_counter: self.likes_counter += 1)
+  end
+
+  def increment_comments_counter
+    update(comments_counter: self.comments_counter += 1)
+  end
+
+  def update_user_post_counter
+    author.increment_posts_counter
+  end
+
+  def most_recent_comments(limit = 5)
+    p 'Hellooo'
+    comments.order(created_at: :desc).limit(limit)
+  end
 end
